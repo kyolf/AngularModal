2567389fe09e72f8ce8830fccc24e732343906c9
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-12-06 12:23:16 -0800

    made the text right size for mobile

--- about/about-controller.js
+++ about/about-controller.js
@@ -3,7 +3,6 @@
 angular
   .module('about')
   .controller('AboutController', AboutController);
-
 function AboutController() {
   var abtVm = this;
   abtVm.titleArr = [
@@ -11,8 +10,10 @@ function AboutController() {
     'MST3K'
   ];
   abtVm.descArr = [
-    'In the not too-distant future, next Sunday, AD',
+    'In the not-too-distant future, next Sunday, AD',
     'There was a man. There were some puppets. It was amazing.',
     'There are the experiments of the SQL.'
   ];
+
+  abtVm.descJoin = abtVm.descArr.join(' ');
 }
